3e6513fe98e14bc2adadd42296d4a1f3291d0d1a
sazeai <the6thsense137@gmail.com>
2024-11-30 10:21:05 +0530

    first committtt

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
+
